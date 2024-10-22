efb28eb87eaf33d2a75cf628748afee9b659dcc7
taleksey <alterehov@gmail.com>
2024-10-22 23:53:57 +0300

    Re init second lesson

---
+++ src/main/java/org/aleksey/lesson2/MoveZero.java
@@ -0,0 +1,33 @@
+package org.aleksey.lesson2;
+
+import java.util.ArrayList;
+
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
+public class MoveZero {
+    public static void main(String[] args) {
+        System.out.println();
+        int[] nums = {0, 1, 0, 3, 12};
+        ArrayList<Integer> result = moveZeroes(nums);
+        System.out.println(result);
+
+        nums = new int[]{0};
+        result = moveZeroes(nums);
+        System.out.println(result);
+    }
+
+    public static ArrayList<Integer> moveZeroes(int[] nums) {
+        ArrayList<Integer> valueList = new ArrayList<>();
+        ArrayList<Integer> zeroValueList = new ArrayList<>();
+        for (int num : nums) {
+            if (num != 0) {
+                valueList.add(num);
+            } else {
+                zeroValueList.add(num);
+            }
+        }
+
+        valueList.addAll(zeroValueList);
+
+        return valueList;
+    }
+}
